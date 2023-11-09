be9c8efdd411803ddad4c3d8de5c8074b29ab29d
khigashinosg <108298890+khigashinosg@users.noreply.github.com>
2023-11-09 18:47:16 +0000

    Added fill_empty_label

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -56,15 +56,22 @@ class Regressor():
         #######################################################################
         #                       ** START OF YOUR CODE **
         #######################################################################
+        
 
-        # Replace this code with your own
-        # Return preprocessed x and y, return None for y if it was None
-        return x, (y if isinstance(y, pd.DataFrame) else None)
 
         #######################################################################
         #                       ** END OF YOUR CODE **
         #######################################################################
 
+    # Correct empty feature instances with a normally distributed random variable using the feature mean and standard deviation
+    def fill_empty_label
+        for label in df.columns: # loop to for all labels
+        mask = df[label].isnull()
+        num_empty = mask.sum()
+        
+        if num_empty > 0: # if null values detected, replace them with a randomly generated variable
+            random_values = np.random.normal(mean, std_dev, size=num_empty)
+            df.loc[mask, label] = random_values
         
     def fit(self, x, y):
         """
